050a4b70e5aacc7569a6706589a77067de3c754c
Coach <ikhotin@intropro.com>
2015-07-13 13:25:47 +0300

    added basic test

---
+++ src/test/java/io/ttl/TotalTest.java
@@ -0,0 +1,12 @@
+package total;
+
+import org.junit.Test;
+
+public class TotalTest {
+
+    @Test
+    public void testHello() {
+        System.out.println("Hello @Test");
+        assert true;
+    }
+}
